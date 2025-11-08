0a592035ce5f726be716cab1cd1e878a4dcc5407
CristianAlvarez <cristianandres1496@gmail.com>
2025-11-07 21:27:14 -0500

    importdata command

--- dataentry/management/commands/importdata.py
+++ dataentry/management/commands/importdata.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from dataentry.models import Student
+import csv
 
 #Proposed command - python manage.py importdata file_path
 #path => C:\Users\-\Desktop\students_data.csv
@@ -11,6 +12,14 @@ class Command(BaseCommand):
     parser.add_argument("file_path", type=str, help="Path to the CSV file")
   
   def handle(self, *args, **kwargs):
-    file_path = kwargs["file_path"]
-    print(file_path)
+    file_path = kwargs['file_path']
+    
+    with open(file_path,'r',encoding='utf-8') as file:
+      reader = csv.DictReader(file)
+      
+      for row in reader:
+        student_exists = Student.objects.filter(roll_no = row['roll_no']).exists()
+        if not student_exists:
+          Student.objects.create(**row)
+          
     self.stdout.write(self.style.SUCCESS("Data inserted successfully!"))
